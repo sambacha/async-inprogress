6ecb454884139406b4071101c887c035477746fd
sam <sam@freighttrust.com>
2020-11-06 02:34:03 -0800

    feat(release): push

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mkdirp dist ; browserify index.js --standalone asyncWhile > dist/async-while.js ; uglifyjs dist/async-while.js > dist/async-while.min.js
+./node_modules/.bin/browserify index.js --standalone asyncWhile > dist/async-while.js ; uglifyjs dist/async-while.js > dist/async-while.min.js
